8e2304af30d53cf2b55105afcd8dc4a91662dcac
Verifyhsjajjsjsj <mykakjsjjsos@gmail.com>
2025-01-29 19:42:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ que = {}
 admins = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("7511275979:AAEwLcIrSW6F-d5t3FEmUiy46l4ag2D8J90")
-BOT_NAME = getenv("BOT_NAME", "ᴄʏʙᴇʀ ᴍᴜsɪᴄ ʙᴏᴛ")
+BOT_NAME = getenv("BOT_NAME", "ᴍᴜsɪᴄ ʙᴏᴛ")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/6790864f5fe27471bdc8d.png")
 THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/e9a4d6655e5ddf51f9160.jpg")
 AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/91034f175d41040d45b38.jpg")
@@ -24,7 +24,7 @@ UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "CyberMusicProject")
 OWNER_NAME = getenv("OWNER_NAME", "Rebel") # isi dengan username kamu tanpa simbol @
 PMPERMIT = getenv("PMPERMIT", None)
 OWNER_ID = int(os.environ.get("6717382350")) # fill with your id as the owner of the bot
-DATABASE_URL = os.environ.get("DATABASE_URL") # fill with your mongodb url
+DATABASE_URL = os.environ.get("mongodb+srv://rebelbotz22:vNcEEoNvSQ33d44K@cluster0.oj1hu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # fill with your mongodb url
 LOG_CHANNEL = int(os.environ.get("-1002259872144")) # make a private channel and get the channel id
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False)) # just fill with True or False (optional)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
